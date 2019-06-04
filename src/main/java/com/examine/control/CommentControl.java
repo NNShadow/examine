9c8c1785c826dc60e9d9cbfebd15d0a25b62b7d8
NNShadow <1581333251@qq.com>
2019-06-04 15:00:21 +0800

    finish

--- src/main/java/com/examine/control/CommentControl.java
+++ src/main/java/com/examine/control/CommentControl.java
@@ -2,7 +2,6 @@ package com.examine.control;
 
 import com.examine.model.Comment;
 import com.examine.service.CommentService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
